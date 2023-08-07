79953c4b3a7d8767a722a124b15a8422e9463347
SturdyBokeh <sturdy.bokeh-0k@icloud.com>
2023-08-07 17:53:52 +0100

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod examples 
+{
+    pub mod ch2_tuples;
+    pub mod ch1_display;
+    pub mod ch1_formatting;
+}
+use examples::*;
+
+
+fn main() {
+    //ch1_display::main();
+    //ch2_tuples::main();
+    //ch1_formatting::main();
+
+}
