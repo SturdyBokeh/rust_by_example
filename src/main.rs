4882258e1157b9796cb9bb3d05ab0d560b19bbbd
SturdyBokeh <sturdy.bokeh-0k@icloud.com>
2023-08-08 16:42:18 +0100

    completed chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#![allow(dead_code)]
 mod examples 
 {
     pub mod ch2_tuples;
     pub mod ch1_display;
     pub mod ch1_formatting;
+    pub mod ch3_structures;
 }
 use examples::*;
 
@@ -11,5 +13,6 @@ fn main() {
     //ch1_display::main();
     //ch2_tuples::main();
     //ch1_formatting::main();
+    //ch3_structures::main();
 
 }
